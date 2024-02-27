a4b40f619ed7bd3bc6cac15ba50cd443fdfb5d1b
Vukasin <vukasin123@gmail.com>
2024-02-27 23:02:47 +0100

    aa

--- sscroll.js
+++ sscroll.js
@@ -1,24 +1,69 @@
 $(document).ready(function() {
-    // Izvlačenje rečenice iz URL parametra
-    var sentence = new URLSearchParams(window.location.search).get('sentence');
- 
-    // Ako postoji rečenica u URL-u
-    if (sentence) {
-        // Zamjena specijalnih znakova za regex
-        sentence = sentence.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
- 
-        // Traženje rečenice na stranici
-        $("body").find("*").each(function() {
-            if ($(this).children().length === 0) { // Provjera da element nema djece, tražimo tekstualni sadržaj
-                var regex = new RegExp(sentence, "i"); // i za case insensitive
-                if (regex.test($(this).text())) {
-                    // Scroll do elementa koji sadrži rečenicu
-                    $('html, body').animate({
-                        scrollTop: $(this).offset().top
-                    }, 1000);
-                    return false; // Prekida each petlju nakon prvog poklapanja
-                }
+    var urlParams = new URLSearchParams(window.location.search);
+    var query = urlParams.get('query');
+    if (query) {
+        $('#query').val(query);
+    }
+    var query = $("#query").val();
+    if (query.length >= 3) {
+        $.ajax({
+            url: 'https://virion.com.hr/results.php',
+            type: 'GET',
+            data: {
+                query: query
+            },
+            success: function(response) {
+                var data = JSON.parse(response);
+                $('#results').empty();
+                data.forEach(function(item) {
+                    var sentence = item.sentence.replace(new RegExp(query, 'g'), '<b style="font-size:21px">' + query + '</b>');
+                    var cleanSentence = $('<div>').html(sentence).text(); // Uklanja HTML tagove iz sentence
+var encodedSentence = encodeURIComponent(cleanSentence); // Kodira 'sentence' za korištenje u URL-u
+var encodedUrl = encodeURI(item.url); // Kodira URL
+
+var $link = $('<a>').attr('href', encodedUrl + '?sentence=' + encodedSentence).text(cleanSentence);
+var $paragraph = $('<p>').append($link);
+
+$('#results').append($paragraph);
+
+                });
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.error(textStatus, errorThrown);
             }
         });
-    }
+        }
+
+    $('#query').keyup(function() {
+        var query = $(this).val();
+    if (query.length >= 3) {
+        $.ajax({
+            url: 'https://virion.com.hr/results.php',
+            type: 'GET',
+            data: {
+                query: query
+            },
+            success: function(response) {
+                var data = JSON.parse(response);
+                console.log(data);
+                $('#results').empty();
+                data.forEach(function(item) {
+                    var sentence = item.sentence.replace(new RegExp(query, 'g'), '<b style="font-size:21px">' + query + '</b>');
+                    var cleanSentence = $('<div>').html(sentence).text(); // Uklanja HTML tagove iz sentence
+var encodedSentence = encodeURIComponent(cleanSentence); // Kodira 'sentence' za korištenje u URL-u
+var encodedUrl = encodeURI(item.url); // Kodira URL
+
+var $link = $('<a>').attr('href', encodedUrl + '?sentence=' + encodedSentence).text(cleanSentence);
+var $paragraph = $('<p>').append($link);
+
+$('#results').append($paragraph);
+
+            });
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.error(textStatus, errorThrown);
+            }
+        });
+        }
+    });
 });
